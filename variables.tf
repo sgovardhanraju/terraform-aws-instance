646eadfea15fdede0a3ed212b6f6ea6ffaf9d233
Govardhan Raju <gsurapur@gmail.com>
2025-11-09 14:06:58 +0400

    session-34

--- variables.tf
+++ variables.tf
@@ -7,6 +7,10 @@ variable "ami_id" {
 variable "instance_type" {
   type = string
   description = "Instance type used for creating EC2 instance"
+  validation {
+    condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Please select either t3 micro or small or medium"
+  }
 }
 
 variable "sg_ids" {
